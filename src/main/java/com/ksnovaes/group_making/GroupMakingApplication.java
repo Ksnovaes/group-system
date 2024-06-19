b00c824b8805c223bb214ff9d9306ed7cb269516
Ksnovaes <kauanovais84@gmail.com>
2024-06-19 19:11:15 -0300

    Inital commit

---
+++ src/main/java/com/ksnovaes/group_making/GroupMakingApplication.java
@@ -0,0 +1,13 @@
+package com.ksnovaes.group_making;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupMakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupMakingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksnovaes/group_making/GroupMakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksnovaes.group_making;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupMakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
